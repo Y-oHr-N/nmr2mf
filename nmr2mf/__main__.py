35643458b60463072bade378258a444d9ea0b7db
Y-oHr-N <kon.y.ohr.n@gmail.com>
2021-12-21 02:45:26 +0900

    Fix KeyError

--- nmr2mf/__main__.py
+++ nmr2mf/__main__.py
@@ -7,63 +7,64 @@ from selenium import webdriver
 
 from .utils import get_stake
 
-options = webdriver.chrome.options.Options()
+if __name__ == "__main__":
+    options = webdriver.chrome.options.Options()
 
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument(
-    "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-    "AppleWebKit/537.36 (KHTML, like Gecko) "
-    "Chrome/93.0.4577.63 "
-    "Safari/537.36"
-)
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument(
+        "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/93.0.4577.63 "
+        "Safari/537.36"
+    )
 
-driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options=options)
 
-driver.get(
-    "https://moneyforward.com/accounts/show_manual/"
-    f"{os.environ['MONEYFORWARD_WALLET_ID']}"
-)
+    driver.get(
+        "https://moneyforward.com/accounts/show_manual/"
+        f"{os.environ['MONEYFORWARD_WALLET_ID']}"
+    )
 
-e = driver.find_element_by_link_text("メールアドレスでログイン")
+    e = driver.find_element_by_link_text("メールアドレスでログイン")
 
-e.click()
+    e.click()
 
-e = driver.find_element_by_xpath("//input[@type='email']")
+    e = driver.find_element_by_xpath("//input[@type='email']")
 
-e.clear()
-e.send_keys(os.environ["MONEYFORWARD_EMAIL"])
+    e.clear()
+    e.send_keys(os.environ["MONEYFORWARD_EMAIL"])
 
-e = driver.find_element_by_xpath("//input[@type='submit']")
+    e = driver.find_element_by_xpath("//input[@type='submit']")
 
-e.click()
+    e.click()
 
-e = driver.find_element_by_xpath("//input[@type='password']")
+    e = driver.find_element_by_xpath("//input[@type='password']")
 
-e.clear()
-e.send_keys(os.environ["MONEYFORWARD_PASSWORD"])
+    e.clear()
+    e.send_keys(os.environ["MONEYFORWARD_PASSWORD"])
 
-e = driver.find_element_by_xpath("//input[@type='submit']")
+    e = driver.find_element_by_xpath("//input[@type='submit']")
 
-e.click()
+    e.click()
 
-e = driver.find_element_by_link_text("残高修正")
+    e = driver.find_element_by_link_text("残高修正")
 
-e.click()
+    e.click()
 
-driver.implicitly_wait(10)
+    driver.implicitly_wait(10)
 
-e = driver.find_element_by_xpath("//input[@id='rollover_info_value']")
+    e = driver.find_element_by_xpath("//input[@id='rollover_info_value']")
 
-stake = get_stake()
+    stake = get_stake()
 
-e.clear()
-e.send_keys(int(stake))
+    e.clear()
+    e.send_keys(int(stake))
 
-e = driver.find_element_by_xpath("//form[@id='rollover_form']/div[5]/div/input")
+    e = driver.find_element_by_xpath("//form[@id='rollover_form']/div[5]/div/input")
 
-e.click()
+    e.click()
 
-driver.implicitly_wait(10)
+    driver.implicitly_wait(10)
 
-driver.quit()
+    driver.quit()
